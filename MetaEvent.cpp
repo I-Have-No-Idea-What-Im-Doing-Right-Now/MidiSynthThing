88fd363634d6aad7845861d66065a46aaa4ea0aa
Leo Pilcher <leo@Leos-MacBook-Air.local>
2026-07-14 13:03:48 -0500

    Finished MetaEvent class for MIDI parser

--- MetaEvent.cpp
+++ MetaEvent.cpp
@@ -21,6 +21,6 @@ TrackEvent(EventType::metaEvent, offset) {
 
 std::string MetaEvent::ToString() const {
 	return std::format(
-	"Meta Event: {}\n\t- Offset: {}\n\t- Data: \n{}",
-	static_cast<int>(metaType), eventOffset, bytes_to_hex(data));
+	"Meta Event: {}\n\t- Offset: {}\n\tData: \n{}",
+	static_cast<int>(metaType), eventOffset, indent_string(bytes_to_hex(data)));
 }
