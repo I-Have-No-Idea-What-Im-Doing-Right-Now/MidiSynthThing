3632b769338d6524de9ba982107430eb373ddb29
Leo Pilcher <pilcher.leo@gmail.com>
2026-07-18 11:06:07 -0500

    Added Sample.midi file

--- main.cpp
+++ main.cpp
@@ -3,18 +3,8 @@
 #include "MetaEvent.h"
 #include "MidiEvent.h"
 #include "MidiTrack.h"
-#include "utils.h"
+#include "MidiSong.h"
 
 int main() {
-	std::vector<TrackEvent*> events;
-	MetaEvent e(20, MetaEventType::copyrightNotice, std::vector<uint8_t>(1));
-	for (int i = 0; i < 20; i++) {
-		e = MetaEvent(20, MetaEventType::copyrightNotice, std::vector<uint8_t>(3));
-		events.push_back(&e);
-	}
-	MidiTrack track(events);
-	std::cout << track.ToString() << std::endl;
-
-	MetaEvent m = MetaEvent(20, MetaEventType::copyrightNotice, std::vector<uint8_t>(3));
-	std::cout << m.ToString() << std::endl;
+	MidiSong x("Sample.midi");
 }
