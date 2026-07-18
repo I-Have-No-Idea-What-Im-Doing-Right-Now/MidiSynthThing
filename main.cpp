ca4db73134d1d575479f46ad5dc1cae4b78696b0
Leo Pilcher <pilcher.leo@gmail.com>
2026-07-18 15:57:42 -0500

    Finished and tested Midi parser

--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
-#include <iostream>
-#include <vector>
-#include "MetaEvent.h"
-#include "MidiEvent.h"
-#include "MidiTrack.h"
 #include "MidiSong.h"
 
 int main() {
-	MidiSong x("Sample.midi");
+	MidiSong x("Midi Files/Sample.midi", true);
 }
